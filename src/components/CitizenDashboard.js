6be6f579f8d559fb88ef7a78bcf9d5201ae7f543
chaitragurjar <chaitra.gurjar@gmail.com>
2024-12-14 22:51:41 +0530

    policy mapping while filinf claims

--- src/components/CitizenDashboard.js
+++ src/components/CitizenDashboard.js
@@ -48,14 +48,14 @@ const CitizenDashboard = () => {
     }
   };
 
-  
+
 
   const fetchPolicy = async () => {
     try {
       const response = await fetch('/policyMapping.json');
       const policyData = await response.json();
       const userPolicies = policyData.patientPolicyMapping.filter(policy => policy.patientID === userId);
-  
+
       const claimsResponse = await fetch('/claimStatus.json');
       const claimsData = await claimsResponse.json();
       const policyApprovedAmounts = claimsData.claimStatus.reduce((acc, claim) => {
@@ -64,24 +64,24 @@ const CitizenDashboard = () => {
         }
         return acc;
       }, {});
-  
+
       const enrichedPolicies = userPolicies.map(policy => ({
         ...policy,
         approvedAmount: policyApprovedAmounts[policy.policyID] || 0,
       }));
-  
+
       setPolicies(enrichedPolicies);
     } catch (error) {
       console.error('Error fetching policies or claims:', error);
     }
   };
-  
+
 
   const policyStatus = () => {
     setPolicyOpen(!policyOpen);
     if (!policyOpen) {
       fetchPolicy();
-      
+
     }
   };
 
@@ -90,20 +90,20 @@ const CitizenDashboard = () => {
     setIsSubmitting(true);
     try {
       const proof = await transfer(userId, parseInt(hospitalID, 10), amount);
-      setProof(proof); 
-      
+      setProof(proof);
+
       console.log("Funds transferred successfully:", proof);
-    
+
       const newEntry = {
         patientID: userId,
         hospitalID: hospitalID,
         policyID: policy,
         claimProof: proof,
-        amount: parseInt(amount,10),
+        amount: parseInt(amount, 10),
         status: 'PENDING'
       };
       console.log(newEntry)
-    
+
       const updatedJson = updateJsonData(newEntry);
       setResult('Claim submitted successfully! ' + proof);
       console.log('Updated JSON:', updatedJson);
@@ -115,8 +115,10 @@ const CitizenDashboard = () => {
   }
 
   useEffect(() => {
+
     const hospitalID = Math.random() < 0.5 ? 1 : 2;
     setHospitalID(hospitalID);
+
     const fetchCitizenData = async () => {
       try {
         const response = await fetch('/patients.json');
@@ -131,8 +133,10 @@ const CitizenDashboard = () => {
         console.error('Error fetching the citizen data:', error);
       }
     };
+
     if (userId !== null) {
       fetchCitizenData();
+      fetchPolicy();
     }
   }, [userId]);
 
@@ -162,47 +166,35 @@ const CitizenDashboard = () => {
           {claimOpen &&
             (<motion.div className='expand'>
               <form onSubmit={submitClaim}>
-              <label>
-                <strong>Policy:</strong>
-                <select value={policy} onChange={(e) => setPolicy(e.target.value)} required>
-                  <option value="" disabled>Select a policy</option>
-                  <option value="PM-JAY">PM-JAY</option>
-                  <option value="MH-JAY">MH-JAY</option>
-                </select>
-              </label>
-              <br /><br />
+                <label> Policy :
+                  <select value={policy} onChange={(e) => setPolicy(e.target.value)} required>
+                    <option value="" disabled>Select a policy</option>
+                    {policies.map((mapping, index) => (
+                      <option key={index} value={mapping.policyID}>{mapping.policyID}</option>
+                    ))}
+                  </select>
+                </label>
+                <br />
+                <label> Hopital ID :
+                  <select value={hospitalName} onChange={(e) => setHospitalName(e.target.value)} required>
+                    <option value="" disabled>Select a Hospital</option>
+                    <option value="Fortis Mumbai">Fortis Research Mumbai</option>
+                    <option value="Hiranandani Mumbai">Hiranandani Hospital Mumbai</option>
+                    <option value="AIIMS Delhi">AIIMS Delhi</option>
+                    <option value="Apollo Bangalore">Apollo HealthCare Bangalore</option>
+                  </select>
+                  {/* <input type="number" value={hospitalID} onChange={(e) => setHospitalID(e.target.value)} required placeholder="Enter Hospital ID" /> */}
+                </label>
 
-              <label>
-                <strong>Hospital ID:</strong>
-                <select value={hospitalName} onChange={(e) => setHospitalName(e.target.value)} required>
-                  <option value="" disabled>Select a Hospital</option>
-                  <option value="Fortis Mumbai">Fortis Research Mumbai</option>
-                  <option value="Hiranandani Mumbai">Hiranandani Hospital Mumbai</option>
-                  <option value="AIIMS Delhi">AIIMS Delhi</option>
-                  <option value="Apollo Bangalore">Apollo HealthCare Bangalore</option>
-                </select>
-              </label>
-              <br /><br />
-
-              <label>
-                <strong>Amount:</strong>
-                <input 
-                  type="number" 
-                  value={amount} 
-                  onChange={(e) => setAmount(e.target.value)} 
-                  required 
-                  placeholder="Enter amount" 
-                />
-              </label>
-              <br /><br />
-
-              <button 
-                type="submit" 
-              >
-                {isSubmitting ? 'Processing...' : 'Click Here to Pay and Submit Claim'}
-              </button>
-            </form>
-
+                <br />
+                <label> Amount :
+                  <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} required placeholder="Enter amount" />
+                </label>
+                <br />
+                <button type="submit" style={{ backgroundColor: isSubmitting ? '#FEEE91' : '#8eecf5' }} >
+                  {isSubmitting ? 'Processing...' : 'Click Here to Pay and Submit Claim'}
+                </button>
+              </form>
               {result && <p>{result}</p>}
             </motion.div>)
           }
@@ -210,7 +202,7 @@ const CitizenDashboard = () => {
 
         <motion.div layout className='citizen-card'>
           <motion.h2 layout='position' onClick={trackClaims}>
-          <img src={trackclaim} alt="Icon" style={{ width: '30px', marginRight: '10px' }} />
+            <img src={trackclaim} alt="Icon" style={{ width: '30px', marginRight: '10px' }} />
             Track Claims
           </motion.h2>
           {statusOpen &&
@@ -221,15 +213,15 @@ const CitizenDashboard = () => {
                     ${claim.status === 'PENDING' ? 'pending' : ''} 
                     ${claim.status === 'APPROVED' ? 'approved' : ''} 
                     ${claim.status === 'REJECTED' ? 'rejected' : ''}`}>
-                  <div className="claim-card">
-                    <h4 className="claim-policy">Policy: {claim.policyID}</h4>
-                    <div className="claim-details">
-                      <p><strong>Hospital ID:</strong> {claim.hospitalID}</p>
-                      <p><strong>Proof:</strong> {claim.claimProof}</p>
-                      <p><strong>Amount:</strong> &#8377;{claim.amount}</p>
-                      <p><strong>Status:</strong> {claim.status}</p>
+                    <div className="claim-card">
+                      <h4 className="claim-policy">Policy: {claim.policyID}</h4>
+                      <div className="claim-details">
+                        <p><strong>Hospital ID:</strong> {claim.hospitalID}</p>
+                        <p><strong>Proof:</strong> {claim.claimProof}</p>
+                        <p><strong>Amount:</strong> &#8377;{claim.amount}</p>
+                        <p><strong>Status:</strong> {claim.status}</p>
+                      </div>
                     </div>
-                  </div>
 
                   </li>
                 ))}
@@ -248,16 +240,16 @@ const CitizenDashboard = () => {
               <ul className='policies'>
                 {policies.map((policy, ind) => (
                   <li key={ind} className="policy-item">
-                  <div className="policy-card">
-                    <div className="policy-header">
-                      <h3 className="policy-id">{policy.policyID} Policy</h3>
+                    <div className="policy-card">
+                      <div className="policy-header">
+                        <h3 className="policy-id">{policy.policyID} Policy</h3>
+                      </div>
+                      <div className="policy-details">
+                        <p><strong>Total Limit:</strong> &#8377;{policy.limitAvailable}</p>
+                        <p><strong>Available:</strong> &#8377;{policy.limitAvailable - policy.approvedAmount}</p>
+                      </div>
                     </div>
-                    <div className="policy-details">
-                      <p><strong>Total Limit:</strong> &#8377;{policy.limitAvailable}</p>
-                      <p><strong>Available:</strong> &#8377;{policy.limitAvailable - policy.approvedAmount}</p>
-                    </div>
-                  </div>
-                </li>
+                  </li>
                 ))}
               </ul>
             </motion.div>)
